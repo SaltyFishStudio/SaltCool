6b656d6609c4790029b1d0b0ea4172f7fe6bb636
FirsD <firsd@outlook.com>
2017-08-28 21:07:39 +0800

    子弹爆炸效果测试，新目录effects

--- run_cool/Assets/scripts/Obstacle_bullet.cs
+++ run_cool/Assets/scripts/Obstacle_bullet.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class Obstacle_bullet : Obstacle {
 
     private float speed = -5.0f;
+    public GameObject boom_effect ;
+    protected override void OnTriggerEnter(Collider otherCollider)
+    {
+        base.OnTriggerEnter(otherCollider);
+        Instantiate(boom_effect, otherCollider.transform.position, Quaternion.identity);
+        Destroy(transform.gameObject);
+    }
 
-	// Use this for initialization
+    // Use this for initialization
     protected override void Start()
     {
         base.Start();
